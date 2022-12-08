570c0228cbd5b025987c3001c781be3f2ec2fc02
dcaleta <72983808+dcaleta@users.noreply.github.com>
2022-12-08 10:38:20 +0100

    Add files via upload

---
+++ ShoppingCart.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class ShoppingCart
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+        
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [Test]
+        public void TheShoppingCartTest()
+        {
+            driver.Navigate().GoToUrl("https://demowebshop.tricentis.com/cart");
+            new SelectElement(driver.FindElement(By.Id("CountryId"))).SelectByText("Croatia");
+            driver.FindElement(By.Id("ZipPostalCode")).Clear();
+            driver.FindElement(By.Id("ZipPostalCode")).SendKeys("31208");
+            driver.FindElement(By.Id("termsofservice")).Click();
+            driver.FindElement(By.Id("checkout")).Click();
+            driver.Navigate().GoToUrl("https://demowebshop.tricentis.com/onepagecheckout");
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
